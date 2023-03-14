60f9ec3f0fb1236b0b6e997df51004968afb49d4
toastedsalad <blazys@pm.me>
2023-03-14 20:59:39 +0200

    Stepping algo

--- Program.cs
+++ Program.cs
@@ -23,11 +23,67 @@ namespace LearningAlgos
             //Console.WriteLine(result);
 
             // FizzBuzz
-            var result = program.FizzBuzz(15);
+            var result = program.FizzBuzzWList(15);
             Console.WriteLine(string.Join("\n", result));
+
+            // NumberOfSteps
+            //var result = program.NumberOfSteps(8);
+            //Console.WriteLine(result);
+
+            // NumberOfSteps
+            //var result = program.NumberOfStepsBitWise(14);
+            //Console.WriteLine(result);
         }
 
-        public List<string> FizzBuzz(int n)
+        public int NumberOfStepsBitWise(int num)
+        {
+            var steps = 0;
+
+            while (num > 0)
+            {   // We apply a bit mask of 00000001 or just 1 to check if the right most bit is 1 or 0
+                // 1 & 1 is 1
+                // 0 & 1 is 0
+                // Because the right most bit allways adds 1 to the total value of bits
+                // when we do get 1 it means the number is odd
+                // and when we get 0 it means the number was even
+                if ((num & 1 ) == 0) 
+                {
+                    // If the number was even we can halve it by shifting all bits 
+                    // to the right by one place.
+                    // It works because binary is power of 2 
+                    // so the previous place is always half of the curent number.
+                    num >>= 1;
+                }
+                else
+                {
+                    num--;
+                }
+
+                steps++;
+            }
+            return steps;
+        }
+
+        public int NumberOfSteps(int num)
+        {
+            var steps = 0;
+
+            while(num > 0)
+            {   // check if divisible by two
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num--;
+                }
+                steps++;
+            }
+            return steps;
+        }
+
+        public List<string> FizzBuzzWList(int n)
         {
             var currentArray = new List<string>(n);
 
@@ -52,6 +108,36 @@ namespace LearningAlgos
                 }
 
                 currentArray.Add(currentString.ToString());
+            }
+
+            return currentArray;
+        }
+
+        public string[] FizzBuzz(int n)
+        {
+            var currentArray = new string[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                var currentString = new StringBuilder();
+
+                Boolean divisibleBy3 = i % 3 == 0;
+                Boolean divisibleBy5 = i % 5 == 0;
+
+                if (divisibleBy3)
+                {
+                    currentString.Append("Fizz");
+                }
+                if (divisibleBy5)
+                {
+                    currentString.Append("Buzz");
+                }
+                if (currentString.Length == 0)
+                {
+                    currentString.Append(i);
+                }
+
+                currentArray[i - 1] = currentString.ToString();
             }
 
             return currentArray;
