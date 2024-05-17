cdccaeef594d583af0a24c4192132d9d8b493d02
toastedsalad <blazys@pm.me>
2024-05-17 10:53:13 +0300

    String alternator

--- Program.cs
+++ Program.cs
@@ -71,6 +71,26 @@ namespace LearningAlgos
             //Console.WriteLine(ransomNote);
         }
 
+        public static void Alternate(string word1, string word2)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int maxLength = Math.Max(word1.Length, word2.Length);
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < word1.Length)
+                {
+                    result.Append(word1[i]);
+                }
+                if (i < word2.Length)
+                {
+                    result.Append(word2[i]);
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
         public bool CanCOnstructRansomNoteHashMap(string ransomNote, string magazine)
         {
             if (magazine.Length < ransomNote.Length)
