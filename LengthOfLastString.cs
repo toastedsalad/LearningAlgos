80cf10f374ed87cbb4701e08b599bd6e91dd67e3
Gediminas Blazys <blazys@pm.me>
2024-09-01 21:01:37 +0300

    Using more compact style of parenthesis.

--- LengthOfLastString.cs
+++ LengthOfLastString.cs
@@ -2,31 +2,23 @@ using System;
 
 namespace LearningAlgos;
 
-public class LengthOfLastString
-{
-    public static int GetLengthOfLastWord(string s)
-    {
+public class LengthOfLastString{
+    public static int GetLengthOfLastWord(string s){
         // Take the length of string. start iterating from the end for string till ' '
         var wordLength = 0;
 
         var i = s.Length - 1;
-        for(; i >= 0 ; i--)
-        {
+        for(; i > 0 ; i--){
             // Check if it is space, if so then continue;
-            if(s[i] == ' ')
-            {
+            if(s[i] == ' '){
                 continue;
             }
-            else
-            {
-                for(; i >= 0; i--)
-                {
-                    if(s[i] != ' ')
-                    {
+            else{
+                for(; i > 0; i--){
+                    if(s[i] != ' '){
                         wordLength++;
                     }
-                    else
-                    {
+                    else{
                         return wordLength;
                     }
                 }
