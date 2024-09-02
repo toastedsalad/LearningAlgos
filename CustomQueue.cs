45dd77f4ec9ab17028f5860f1311d04c4d27de07
Gediminas Blazys <blazys@pm.me>
2024-09-02 07:00:19 +0300

    Changing style of parenthesis.

--- CustomQueue.cs
+++ CustomQueue.cs
@@ -2,38 +2,31 @@ using System;
 
 namespace LearningAlgos;
 
-public class CustomQueue<T>
-{
+public class CustomQueue<T>{
     private Node<T>? _head;
     private Node<T>? _tail;
     public int Length { get; private set; }
 
-    public CustomQueue()
-    {
+    public CustomQueue(){
         _head = _tail = null;
         Length = 0;
     }
 
-    public void Enqueue(Node<T> item)
-    {
-        if (Length == 0)
-        {
+    public void Enqueue(Node<T> item){
+        if (Length == 0){
             _head = item;
             _tail = _head;
             Length++;
         }
-        else
-        {
+        else{
             _tail.NextNode = item;
             _tail = item;
             Length++;
         }
     }
 
-    public T? Deque()
-    {
-        if (_head is null)
-        {
+    public T? Deque(){
+        if (_head is null){
             return default(T);
         }
 
@@ -46,33 +39,27 @@ public class CustomQueue<T>
         return dequeingNode.NodeData;
     }
 
-    public T? Peek()
-    {
+    public T? Peek(){
         return _head.NodeData;
     }
 }
 
-public class Node<T>
-{
+public class Node<T>{
     public T NodeData { get; set; }
     public Node<T>? NextNode { get; set; }
 
-    public Node(T nodeData, Node<T>? nextNode = null)
-    {
+    public Node(T nodeData, Node<T>? nextNode = null){
         NodeData = nodeData;
         NextNode = nextNode;
     }
 }
 
 
-public static class CustomQueueHelpers<T>
-{
-    public static void PrintLinkedList(Node<T> headNode)
-    {
+public static class CustomQueueHelpers<T>{
+    public static void PrintLinkedList(Node<T> headNode){
         Console.WriteLine("Printing linked list...");
         var temp = headNode;
-        while (temp is not null)
-        {
+        while (temp is not null){
             Console.WriteLine(temp.NodeData);
             temp = temp.NextNode;
         }
