51378c4b2cc04f87a7642d1056f1d97ee74a24d5
toastedsalad <blazys@pm.me>
2024-11-15 10:55:27 +0200

    Implementing ring buffer.

--- Algos/MyCircularQueue.cs
+++ Algos/MyCircularQueue.cs
@@ -10,20 +10,24 @@ namespace LearningAlgos{
             circularArray = new int[k];
             // When we initialize we can set Front and Rear
             frontIndex = rearIndex = 0;
-            circularArray[frontIndex] = -1;
         }
 
         public bool EnQueue(int value) {
+            circularArray[rearIndex] = value;
+            rearIndex++;
             return true;
         }
 
         public bool DeQueue() {
+            frontIndex++;
             return true;
         }
 
         public int Front() {
+            if (IsEmpty()) {
+                return -1;
+            }
             return circularArray[frontIndex];
-            // And if it is empty, we need to return -1.
         }
 
         public int Rear() {
@@ -31,10 +35,18 @@ namespace LearningAlgos{
         }
 
         public bool IsEmpty() {
-            return true;
+            if (frontIndex == rearIndex) {
+                return true;
+            }
+            //        ht
+            // [1, 2, 3]
+            return false;
         }
 
         public bool IsFull() {
+            //  t  h  
+            // [1, 2, 3]
+            // if tail + 1 is head then array is full
             return true;
         }
     }
--- AlgosTests/RingBufferTests.cs
+++ AlgosTests/RingBufferTests.cs
@@ -2,12 +2,10 @@ using LearningAlgos;
 
 namespace AlgosTests{
     public class Tests{
-        [SetUp]
-        public void Setup() {
-        }
+        const int someValue = 7;
 
         [Test]
-        public void NewArrayRetursnEmpty() {
+        public void NewBufferRetursnEmpty() {
             // Randomize the argument? So that it isn't always one?
             var queue = new MyRingBuffer(1);
             var front = queue.Front();
@@ -15,10 +13,53 @@ namespace AlgosTests{
         }
 
         [Test]
-        public void EmptyReturnMinusOne(){
+        public void FrontReturnsFrontValueOfTheArray() {
+            // Randomize the argument? So that it isn't always one?
+            var queue = new MyRingBuffer(2);
+            queue.EnQueue(someValue);
+            var front = queue.Front();
+            Assert.That(front, Is.EqualTo(someValue));
+        }
+
+        [Test]
+        public void DequeRemovesFrontAndReturnsTrue() {
+            // Randomize the argument? So that it isn't always one?
+            // Assert that after deque the rear is rear - 1
+            var queue = new MyRingBuffer(2);
+            queue.EnQueue(someValue);
+            // h    t
+            // [7, 0]
+            var dequeResult = queue.DeQueue();
+            //     ht
+            // [7, 0]
+            Assert.That(dequeResult, Is.EqualTo(true));
+            Assert.That(queue.IsEmpty(), Is.EqualTo(true));
+            Assert.That(queue.Front(), Is.EqualTo(-1));
+            Assert.That(queue.Rear(), Is.EqualTo(-1));
+        }
+
+        [Test]
+        // Add queue and dequeue and then test for emptiness.
+        public void FrontReturnsMinusOneWhenEmpty() {
+            // Randomize the argument? So that it isn't always one?
+            var queue = new MyRingBuffer(2);
+            var front = queue.Front();
+            Assert.That(front, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void EmptyBufferReturnsIsEmptyTrue(){
             var queue = new MyRingBuffer(1);
             var empty = queue.IsEmpty();
             Assert.That(empty, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsEmptyReturnsFalseWhenArrayIsNotEmpty(){
+            var queue = new MyRingBuffer(2);
+            queue.EnQueue(someValue);
+            var empty = queue.IsEmpty();
+            Assert.That(empty, Is.EqualTo(false));
         }
     }
 }
