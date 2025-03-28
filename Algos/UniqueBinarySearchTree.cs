6ffc98c07e6355f9a5279fa75b1c3a1ccab21282
Gediminas Blazys <blazys@pm.me>
2025-03-28 11:55:23 +0200

    Unique binary trees.

---
+++ Algos/UniqueBinarySearchTree.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace LearningAlgos;
+
+public class UniqueBinarySearchtree {
+    public IList<TreeNode> GenerateTrees(int n) {
+        // Binary search trees are trees that 
+        // 1. Values less than root on the left
+        // 2. Values bigger than root on the right
+        
+        
+
+        return default!;
+    }
+}
