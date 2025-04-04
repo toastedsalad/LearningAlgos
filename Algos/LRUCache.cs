4e0a41cc41166d6ed25de4ba42b39b783c42c814
Gediminas Blazys <blazys@pm.me>
2025-04-04 11:00:51 +0300

    Some progress but almost feels like a regression.

--- Algos/LRUCache.cs
+++ Algos/LRUCache.cs
@@ -28,7 +28,18 @@ public class LRUCache {
             // Refer to our drawing.
             var current = node;
             var previous = current.prev;
+            
+            current.next = null;
+            current.prev.next = current.next;
+            current.prev = _tail;
+
+
             previous.next = current.next;
+
+            current.next.prev = current.prev;
+            current.next.next = current.next;
+
+
             _tail.next = node;
             _tail = node;
             node.valueContainer.value = value;
@@ -37,6 +48,7 @@ public class LRUCache {
             var newNode = new DoublyListNode<(int key, int value)>(newValue, null!, _tail);
             dict.Add(key, newNode);
             _tail.next = newNode;
+            newNode.prev = _tail;
             _tail = newNode;
         }
 
